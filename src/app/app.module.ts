e82eb5c7c00e37c3a9594f574729241760d82c8f
pamnanaimanish169 <manishpamnani169@gmail.com>
2020-07-18 12:59:31 +0530

    Integrate: Integrate making Card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AqiComponent } from './aqi/aqi.component';
 import { AqiService } from './aqi.service';
+
+const appRoutes = [
+  { path: '', redirectTo: 'aqi', pathMatch: 'full'},
+  { path: 'aqi', component: AqiComponent}
+];
 
 @NgModule({
   declarations: [
@@ -13,7 +19,8 @@ import { AqiService } from './aqi.service';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [AqiService],
   bootstrap: [AppComponent]
